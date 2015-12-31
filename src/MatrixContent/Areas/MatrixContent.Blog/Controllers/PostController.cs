95e991ea870af3be03703c725a53dd18e51b6b27
AndrewLang <nnlyx@hotmail.com>
2015-12-31 15:58:16 -0700

    Use db context in controller

--- src/MatrixContent/Areas/MatrixContent.Blog/Controllers/PostController.cs
+++ src/MatrixContent/Areas/MatrixContent.Blog/Controllers/PostController.cs
@@ -2,14 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MatrixContent.Framework;
 using Microsoft.AspNet.Mvc;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace MatrixContent.Blog.Controllers
 {
+    [Area("Blog")]
     public class PostController : Controller
     {
+        public PostController(ApplicationDbContext context)
+        {
+
+        }
         // GET: /<controller>/
         public IActionResult Index()
         {
