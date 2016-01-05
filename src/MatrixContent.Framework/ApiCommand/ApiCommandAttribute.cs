5b3391b1e04f4e592aad76eaccb3685bb9c1bbea
AndrewLang <nnlyx@hotmail.com>
2016-01-04 17:03:31 -0700

    Support command discovery

---
+++ src/MatrixContent.Framework/ApiCommand/ApiCommandAttribute.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MatrixContent.Framework
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class ApiCommandAttribute:Attribute
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ApiCommandAttribute"/> class.
+        /// </summary>
+        public ApiCommandAttribute()
+        {
+
+        }
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ApiCommandAttribute"/> class.
+        /// </summary>
+        /// <param name="name">The name.</param>
+        public ApiCommandAttribute(string name)
+        {
+            Name = name;
+        }
+        /// <summary>
+        /// Gets or sets the name.
+        /// </summary>
+        /// <value>
+        /// The name.
+        /// </value>
+        public string Name { get; set; }
+    }
+}
---
+++ src/MatrixContent.Framework/ApiCommand/ApiCommandExtensions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace MatrixContent.Framework
+{
+    public static class ApiCommandExtensions
+    {
+        static BindingFlags DiscoveryFlag = BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic;
+
+        /// <summary>
+        /// Discoveries the commands.
+        /// </summary>
+        /// <param name="type">The type.</param>
+        /// <returns></returns>
+        public static IEnumerable<KeyValuePair<string,Func<object,object>>> DiscoveryCommands(this object instance)
+        {
+            var type = instance.GetType();
+            var commands = type.GetMethods(DiscoveryFlag)
+                     .Where(x => x.GetCustomAttributes(typeof(ApiCommandAttribute),false).Length > 0)
+                     .Select(x =>
+                     {
+                         var attribute = x.GetCustomAttribute<ApiCommandAttribute>(false);
+                         if(!string.IsNullOrEmpty(attribute.Name))
+                         {
+                             // mCommands.Add(attribute.Name,args => { return x.Invoke(this,new object[] { args }); });
+                             return new KeyValuePair<string,Func<object,object>>(attribute.Name,args => { return x.Invoke(instance,new object[] { args }); });
+                         }
+                     });
+
+            return commands;
+        }
+    }
+}
